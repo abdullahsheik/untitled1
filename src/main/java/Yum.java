131f60d064ce15f3c8453b1c172bee3984d7a529
DESKTOP-Q425P0C\Abdullah <abdullahmustafavi@gmail.com>
2022-07-19 13:21:54 -0600

    Initial commit

---
+++ src/main/java/Yum.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class Yum {
+
+    @Test(priority = 1)
+    void setup(){
+        System.out.println("I am inside setup");
+    }
+    @Test(priority = 2)
+    void testSteps(){
+        System.out.println("I am inside testSteps");
+    }
+    @Test(priority = 3)
+    void tearDown(){
+        System.out.println("I am inside tearDown");
+    }
+}
